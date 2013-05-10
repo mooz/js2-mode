3c69aea0c267e7bbadd5e35eb6cab54764c9d91c
Dmitry Gutov <dgutov@yandex.ru>
2013-05-10 19:20:11 +0400

    Bump the version date

--- js2-mode.el
+++ js2-mode.el
@@ -7,7 +7,7 @@
 ;;         Dmitry Gutov <dgutov@yandex.ru>
 ;; URL:  https://github.com/mooz/js2-mode/
 ;;       http://code.google.com/p/js2-mode/
-;; Version: 20130307
+;; Version: 20130510
 ;; Keywords: languages, javascript
 ;; Package-Requires: ((emacs "24.1"))
 
